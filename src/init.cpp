3633376107969ba3e907168108008f2ba6eaf621
Ben Monteiro <42271200+BenCryptoKing@users.noreply.github.com>
2020-03-27 23:35:27 +0100

    Update init.cpp

--- src/init.cpp
+++ src/init.cpp
@@ -555,10 +555,10 @@ void SetupServerArgs()
 
 std::string LicenseInfo()
 {
-    const std::string URL_SOURCE_CODE = "<https://github.com/stephanpower-project/stephanpower>";
-    const std::string URL_WEBSITE = "<https://stephanpower.org>";
+    const std::string URL_SOURCE_CODE = "<https://github.com/bencryptoking/stephanpower>";
+    const std::string URL_WEBSITE = "<https://stephanpower.net>";
 
-    return CopyrightHolders(strprintf(_("Copyright (C) %i-%i"), 2011, COPYRIGHT_YEAR) + " ") + "\n" +
+    return CopyrightHolders(strprintf(_("Copyright (C) %i-%i"), 2017, COPYRIGHT_YEAR) + " ") + "\n" +
            "\n" +
            strprintf(_("Please contribute if you find %s useful. "
                        "Visit %s for further information about the software."),
